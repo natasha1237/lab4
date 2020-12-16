ab89df7d1ef2892ded29f3f7cf0ec8ffa0935154
natasha1237 <45146473+natasha1237@users.noreply.github.com>
2020-12-17 00:07:02 +0200

    First commit

---
+++ lab4/Algorithm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab4
+{
+    class Algorithm
+    {
+        // Список посещенных вершин
+        private HashSet<Element> visited;
+        // Путь из начальной вершины в целевую.
+        private LinkedList<Element> path;
+        private Element goal;
+
+        public LinkedList<Element> A1(Element start, Element goal)
+        {
+            visited = new HashSet<Element>();
+            path = new LinkedList<Element>();
+            this.goal = goal;
+            A1(start);
+            if (path.Count > 0)
+            {
+                path.AddFirst(start);
+            }
+            return path;
+        }
+
+        private bool A1(Element Element)
+        {
+            if (Element == goal)
+            {
+                return true;
+            }
+            visited.Add(Element);
+            foreach (var item in Element.AdjacentEl.Where(x => !visited.Contains(x)))
+            {
+                if (A1(item))
+                {
+                    path.AddFirst(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ lab4/Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab4
+{
+    class Element
+    {
+        public string ElementName { get; }
+
+        // Список сусідніх елементів.
+        public List<Element> AdjacentEl { get; }
+
+        public Element(string name)
+        {
+            ElementName = name;
+            AdjacentEl = new List<Element>();
+        }
+
+        // Додає нову сусідню вершину.
+        public Element AddAdjacentEl(Element Element, bool bidirect = true)
+        {
+            AdjacentEl.Add(Element);
+            if (bidirect)
+            {
+                Element.AdjacentEl.Add(this);
+            }
+            return this;
+        }
+    }
+}
